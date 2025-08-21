f32e6101c30ea392e2d542f09dfd4d7c9da917d4
emmabeni27 <larisabeni@mail.com>
2025-08-21 17:45:40 -0300

    cambios de front nada mas

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,17 +5,17 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <div className="p-4 flex gap-4 border-b bg-background">
-        <h1 className="text-xl font-semibold text-primary">Organizador Diario</h1>
+        <h1 className="text-xl font-semibold text-primary italic">Organizador Diario</h1>
         <div className="flex gap-2 ml-auto">
           <Link
             to="/"
-            className="px-3 py-2 rounded hover:bg-accent [&.active]:bg-primary [&.active]:text-primary-foreground"
+            className="px-3 py-2 rounded hover:bg-accent [&.active]:bg-green-500 [&.active]:text-primary-foreground"
           >
             Home
           </Link>
           <Link
-            to="/about"
-            className="px-3 py-2 rounded hover:bg-accent [&.active]:bg-primary [&.active]:text-primary-foreground"
+            to="/"
+            className="px-3 py-2 rounded hover:bg-accent [&.active]:bg-blue-500 [&.active]:text-primary-foreground"
           >
             About
           </Link>
