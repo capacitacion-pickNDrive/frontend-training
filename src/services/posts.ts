83b443cfccad338f6bfcca8a250c4a03f4b13685
tomasfreile <freiletomy@gmail.com>
2025-08-20 22:42:15 -0300

    refactor: remove type annotations from getPosts and getPost functions

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,12 +1,12 @@
 import type { Post } from '@/types/api'
 import { api } from './api'
 
-export const getPosts = async (): Promise<Post[]> => {
+export const getPosts = async () => {
   const response = await api.get<Post[]>('/posts')
   return response.data
 }
 
-export const getPost = async (id: number): Promise<Post> => {
+export const getPost = async (id: number) => {
   const response = await api.get<Post>(`/posts/${id}`)
   return response.data
 }
